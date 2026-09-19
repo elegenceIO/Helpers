6c7742c2bf48deec1db8c98221134f68e60973fe
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2026-09-19 09:47:13 +0100

    Added Helper class

---
+++ Helpers.php
@@ -0,0 +1,50 @@
+<?php
+namespace ElegenceIO\Core\Helpers;
+use DirectoryIterator;
+use Exception;
+
+class Helpers
+{
+    private array $helpers = [];
+    private string $base = "";
+    public function __construct()
+    {
+        $this->add(rtrim(dirname(__DIR__,2),\DIRECTORY_SEPARATOR).\DIRECTORY_SEPARATOR."Files/");
+    }
+
+    public function add(string $path):void
+    {
+        $this->helpers[] = $path;
+    }
+
+    public function reload()
+    {
+        foreach($this->helpers as $helpers)
+        {
+            // Detects if the item is a file and goes to the base directory and loads all files.
+            // if folder just defaults and scans the directory.
+            $helper = is_file($helpers) ? dirname($helpers) : $helpers;
+            
+            if(!Directories::has($helper))
+            {
+                throw new Exception("Directory Helper not found");
+            }
+            
+            foreach(new DirectoryIterator($helper) as $item)
+            {
+            if($item->isDot()) continue;
+                if(!$item->isFile()) continue;
+                $filepath = $item->getFileInfo();
+                [$name,$ext] = explode(".",$filepath);
+                $name = \ucfirst($name);
+
+                if(!isset($this->helper[$name])){
+                    $this->helpers[$name] = $filepath;
+                    include_once($this->helpers[$name]);
+                }
+            }
+        }
+    }
+
+
+}
